8f9dcb114a7ade29e90fc8a416e925e23fe06eba
00MY00 <75865145+00MY00@users.noreply.github.com>
2021-12-23 23:34:18 +0100

    Add files via upload

---
+++ find-mp3.bat
@@ -0,0 +1,42 @@
+@echo off
+color a
+title Find MP3
+del /f t.txt
+del /f mp3Find.txt
+echo.[40;35m
+echo ███████╗██╗███╗   ██╗██████╗     ███╗   ███╗██████╗ ██████╗ 
+echo ██╔════╝██║████╗  ██║██╔══██╗    ████╗ ████║██╔══██╗╚════██╗
+echo █████╗  ██║██╔██╗ ██║██║  ██║    ██╔████╔██║██████╔╝ █████╔╝
+echo ██╔══╝  ██║██║╚██╗██║██║  ██║    ██║╚██╔╝██║██╔═══╝  ╚═══██╗
+echo ██║     ██║██║ ╚████║██████╔╝    ██║ ╚═╝ ██║██║     ██████╔╝
+echo ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝     ╚═╝     ╚═╝╚═╝     ╚═════╝ 
+echo.[40;36m
+
+echo [40;37mpermet de transvaser les fichier .mp3 du dossier [40;33mDownloads[40;37m
+pause
+
+cls
+
+
+if exist %USERPROFILE%\Desktop\mp3 goto next
+
+mkdir %USERPROFILE%\Desktop\mp3
+
+
+:next
+
+cd %USERPROFILE%\Downloads
+
+
+
+copy *.mp3 %USERPROFILE%\Desktop\mp3 /Y
+
+
+pause
+
+del *.mp3
+
+start %USERPROFILE%\Desktop\mp3
+
+
+
